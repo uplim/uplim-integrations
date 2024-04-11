f9c0d96d13e38103c9541be9534c5de691aaca4f
Daiki Maekawa <k1tikurisu@gmail.com>
2024-04-11 18:59:37 +0900

    monitoring: fox

--- packages/uplim-kun-monitoring/src/main.ts
+++ packages/uplim-kun-monitoring/src/main.ts
@@ -1,19 +1,6 @@
-import { withTimeout } from "utils";
-
-declare const global: Record<string, () => void>;
 global.main = main;
 
 function main() {
   const url = "https://uplim-kun.onrender.com";
-  try {
-    withTimeout(
-      new Promise((resolve) => {
-        UrlFetchApp.fetch(url).getResponseCode();
-        resolve("");
-      }),
-      1000,
-    );
-  } catch {
-    // 何もしない
-  }
+  UrlFetchApp.fetch(url).getResponseCode();
 }
